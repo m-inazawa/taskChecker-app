3e12814f34bcbe494b5f13c2e83a30acd3cf6470
m-inazawa <m.inazawa@infomart.co.jp>
2025-12-23 18:01:52 +0900

    ルートコンポーネント作成

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,11 @@
+// import React from "react";
+import "./style.css";
+import { Header } from "../../_components/header";
+
+export const Home = () => {
+  return (
+  <div className="main">
+    <Header></Header>
+  </div>
+  );
+};
