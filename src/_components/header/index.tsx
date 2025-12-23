81a43975072518debc97e3ce488308b8a4f97312
m-inazawa <m.inazawa@infomart.co.jp>
2025-12-23 18:02:10 +0900

    Header作成

---
+++ src/_components/header/index.tsx
@@ -0,0 +1,11 @@
+import DoneAllIcon from '@mui/icons-material/DoneAll';
+import './style.css';
+
+export const Header = () => {
+  return (
+    <div className="header">
+      <DoneAllIcon className="header_icon"></DoneAllIcon>
+      <span className="header_title">Task Checker</span>
+    </div>
+  )
+}
---
+++ src/_components/select/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./style.css"
+
+export const Select = () => {
+  return (
+    <Form></Form>//仮
+  )
+}
